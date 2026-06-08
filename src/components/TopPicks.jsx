06107d622a878a9cecee0a03cd041e79c58ad8de
prem-choithani23 <premchoithani4@gmail.com>
2026-06-08 13:15:55 +0530

    feat(components): update author links to use author ID across all card components

--- src/components/TopPicks.jsx
+++ src/components/TopPicks.jsx
@@ -1,10 +1,17 @@
-import React from "react";
-import {Link} from "react-router-dom";
-import {getTopPicks} from "../data/blogSelector.js";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { getFeaturedPosts } from "../api/services/postService.js";
+import { pastelColorFromString, lightenHsl } from "../utils/color.js";
 
 export default function TopPicks({ className = "" }) {
+    const [picks, setPicks] = useState([]);
 
-    const topPicksData = getTopPicks();
+    useEffect(() => {
+        getFeaturedPosts(5)
+            .then((res) => setPicks(res.data.content))
+            .catch(console.error);
+    }, []);
+
     return (
         <div className={className}>
             <h4 className="font-bold mb-4 text-[22px] text-[#3b3f45] dark:text-white">
@@ -12,35 +19,36 @@ export default function TopPicks({ className = "" }) {
             </h4>
 
             <div className="flex flex-col gap-5">
-                {topPicksData.map((blog, index) => (
-                    <div key={index} className="flex gap-4 items-start">
-                        <div
-                            style={{ backgroundColor: blog.upperColor }}
-                            className="w-[90px] h-[90px] rounded-md flex-shrink-0"
-                        >
-                            {blog.imageUrl && (
-                                <img
-                                    src={blog.imageUrl}
-                                    alt={blog.title}
-                                    className="w-full h-full object-cover rounded-md"
-                                />
-                            )}
+                {picks.map((post) => {
+                    const upperColor = lightenHsl(pastelColorFromString(post.id.toString()), 6);
+                    return (
+                        <div key={post.id} className="flex gap-4 items-start">
+                            <div
+                                style={{ backgroundColor: upperColor }}
+                                className="w-[90px] h-[90px] rounded-md flex-shrink-0"
+                            >
+                                {post.featuredImageUrl && (
+                                    <img
+                                        src={post.featuredImageUrl}
+                                        alt={post.title}
+                                        className="w-full h-full object-cover rounded-md"
+                                    />
+                                )}
+                            </div>
+
+                            <div className="flex flex-col">
+                                <Link to={`/blogs/${post.slug}`}>
+                                    <p className="hover:underline text-[18px] font-semibold leading-snug text-[#3b3f45] dark:text-white">
+                                        {post.title}
+                                    </p>
+                                </Link>
+                                <span className="text-[13px] text-[#94979e] mt-1">
+                                    {post.viewCount} Views
+                                </span>
+                            </div>
                         </div>
-
-                        <div className="flex flex-col">
-                            <Link to={`/blogs/${blog.slug}`}>
-                                <p className="hover:underline text-[18px] font-semibold leading-snug text-[#3b3f45] dark:text-white">
-                                    {blog.title}
-                                </p>
-                            </Link>
-
-
-                            <span className="text-[13px] text-[#94979e] mt-1">
-                                {blog.views} Views
-                            </span>
-                        </div>
-                    </div>
-                ))}
+                    );
+                })}
             </div>
         </div>
     );

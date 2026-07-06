e9252b9538e0178bbbc92396d87937a78ba2af2a
prem-choithani <premchoithani4@gmail.com>
2026-07-07 02:49:07 +0530

    feat(image): add all paths to all server side static images

--- src/data/blog_images_data.js
+++ src/data/blog_images_data.js
@@ -1,26 +1,20 @@
-// Blog Images
-import Img1 from "../../public/assets/blog_images/koga-featured-1-scaled-1-1536x1536.webp";
-import Img2 from "../../public/assets/blog_images/koga-featured-2-440x440.webp";
-import Img3 from "../../public/assets/blog_images/koga-featured-2-768x768.webp";
-import Img4 from "../../public/assets/blog_images/koga-featured-3-440x440.webp";
-import Img5 from "../../public/assets/blog_images/koga-featured-3-768x768.webp";
-import Img6 from "../../public/assets/blog_images/koga-featured.4-scaled-1-1536x1536.webp";
-import Img7 from "../../public/assets/blog_images/koga-featured-5-768x768.webp";
-import Img8 from "../../public/assets/blog_images/koga-featured-6-440x440.webp";
-import Img9 from "../../public/assets/blog_images/koga-featured-6-768x768.webp";
-import Img10 from "../../public/assets/blog_images/koga-featured-6-1024x1024.webp";
-import Img11 from "../../public/assets/blog_images/koga-featured-7-768x768.webp";
-import Img12 from "../../public/assets/blog_images/koga-featured-8-440x293.webp";
-import Img13 from "../../public/assets/blog_images/koga-featured-9-scaled-1-1200x1200.webp";
-import Img14 from "../../public/assets/blog_images/koga-featured-11-440x440.webp";
-import Img15 from "../../public/assets/blog_images/koga-featured-14-scaled-1-768x768.webp";
-import Main1 from "../../public/assets/blog_images/main1.webp";
-
 const blogImagesData = [
-    Img1, Img2, Img3, Img4, Img5,
-    Img6, Img7, Img8, Img9, Img10,
-    Img11, Img12, Img13, Img14, Img15,
-    Main1,
+    "/assets/blog_images/koga-featured-1-scaled-1-1536x1536.webp",
+    "/assets/blog_images/koga-featured-2-440x440.webp",
+    "/assets/blog_images/koga-featured-2-768x768.webp",
+    "/assets/blog_images/koga-featured-3-440x440.webp",
+    "/assets/blog_images/koga-featured-3-768x768.webp",
+    "/assets/blog_images/koga-featured.4-scaled-1-1536x1536.webp",
+    "/assets/blog_images/koga-featured-5-768x768.webp",
+    "/assets/blog_images/koga-featured-6-440x440.webp",
+    "/assets/blog_images/koga-featured-6-768x768.webp",
+    "/assets/blog_images/koga-featured-6-1024x1024.webp",
+    "/assets/blog_images/koga-featured-7-768x768.webp",
+    "/assets/blog_images/koga-featured-8-440x293.webp",
+    "/assets/blog_images/koga-featured-9-scaled-1-1200x1200.webp",
+    "/assets/blog_images/koga-featured-11-440x440.webp",
+    "/assets/blog_images/koga-featured-14-scaled-1-768x768.webp",
+    "/assets/blog_images/main1.webp",
 ];
 
-export default blogImagesData
+export default blogImagesData;

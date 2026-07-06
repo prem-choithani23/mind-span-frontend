8aa32c0a40d913da430679c195aae0135469cf9b
prem-choithani <premchoithani4@gmail.com>
2026-07-07 02:46:50 +0530

    refactor : Wrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { ThemeProvider } from "./context/ThemeContext";
-import "./index.css"
+import "./index.css";
+
 import AuthProvider from "./context/AuthContext";
+import { ThemeProvider } from "./context/ThemeContext";
+import ToastProvider from "./context/ToastContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <AuthProvider>
-            <ThemeProvider>
-                <App />
-            </ThemeProvider>
-        </AuthProvider>
-        
-    </React.StrictMode>
+  <React.StrictMode>
+    <AuthProvider>
+      <ThemeProvider>
+        <ToastProvider>
+          <App />
+        </ToastProvider>
+      </ThemeProvider>
+    </AuthProvider>
+  </React.StrictMode>
 );

eb8f5b7f2ce59aff22e906e8584b06641a463611
prem-choithani <premchoithani4@gmail.com>
2026-07-07 20:00:35 +0530

    reactor: add rendomized category selection and use modified components with correct components

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -4,20 +4,56 @@ import EditorsPicksSection from "./section/EditorsPicksSection.jsx";
 import CategoryBlogSection from "./section/CategoryBlogSection.jsx";
 import HeroSection from "./section/HeroSection.jsx";
 import SliderCardGrouping from "./section/SliderCardGrouping.jsx";
-
+import { useState , useEffect } from "react";
+import { getAllCategories } from "../api/services/categoryService.js";
 export default function Landing() {
+
+    const [randomCategories, setRandomCategories] = useState([]);
+
+    useEffect(() => {
+        const load = async () => {
+            const res = await getAllCategories();
+
+            const shuffled = [...res.data].sort(() => Math.random() - 0.5);
+
+
+            setRandomCategories(shuffled);
+
+            // console.log("RANDOM: " + randomCategories)
+
+    };
+
+    load();
+}, []);
     return (
         <div className="pt-8 lg:pt-11 xl:pt-15">
             <HeroSection />
 
-            <SliderCardGrouping type="Latest Posts" isCategory={false} />
+            <SliderCardGrouping
+                type={"Latest Posts"}
+                isCategory:false
+            />
+
+            <SliderCardGrouping
+                categoryId={randomCategories[1]?.id}
+                type={randomCategories[1]?.name}
+                isCategory
+            />
 
             <CategoryBlogSection
-                categoryId={2}
-                categoryName="Web Development"
+                categoryId={randomCategories[2]?.id}
+                categoryName={randomCategories[2]?.name}
+            />
+
+            <SliderCardGrouping
+                categoryId={randomCategories[3]?.id}
+                type={randomCategories[3]?.name}
+                isCategory
             />
 
             <EditorsPicksSection />
+
+            <FeaturedSection />
 
             <FeaturedSection />
         </div>

045f3634962aca75fe9dbfc6c0a326735d22d575
prem-choithani <premchoithani4@gmail.com>
2026-07-11 12:47:12 +0530

    refactor: use tailwindcss typography for markdown writing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import { requestFormReset } from "react-dom";
+
 //** @type {import('tailwindcss').Config} */
 export default {
     darkMode: "class",
@@ -9,5 +11,7 @@ export default {
             },
         },
     },
-    plugins: [],
+    plugins: [
+        require("@tailwindcss/typography"),
+    ]
 };
